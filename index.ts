b642553c1fcf011c8c7ba946ac97324b7582a678
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-11-27 14:25:37 +0100

    Add new files and update .gitignore

---
+++ index.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from 'express';
+import { config } from 'dotenv';
+import path from 'path';
+
+config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+
+app.use(express.json());
+app.use(express.static('pages/login'));
+
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile('login.html', { root: path.join(__dirname, '..', 'pages', "login") });
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
